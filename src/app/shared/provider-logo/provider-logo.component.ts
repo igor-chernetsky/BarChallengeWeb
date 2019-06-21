0e308d0df53aba134929906d1d5b99b02650eab7
Igor <blackzzz.9@gmail.com>
2019-06-21 15:18:35 +0300

    styles were updated

--- src/app/shared/provider-logo/provider-logo.component.ts
+++ src/app/shared/provider-logo/provider-logo.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProviderLogoComponent implements OnInit {
   @Input() public logo: string;
+  @Input() public small: boolean;
 
   constructor() { }
 
